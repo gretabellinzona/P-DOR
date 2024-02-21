38c2ee220fbb81d533f8d37179ccbf81864c8e0b
Greta Bellinzona <79328996+gretabellinzona@users.noreply.github.com>
2024-02-21 16:00:37 +0100

    Create python package

---
+++ P-DOR/__init__.py
@@ -0,0 +1,9 @@
+"""
+P-DOR - a Pipeline to Disentangle Outbreaks Rapidly
+
+P-DOR is a bioinformatic pipeline for rapid WGS-based bacterial outbreak detection and characterization, carried on by integrating clinical metadata and contextualizing the genomes of interest within a well curated global genomic database.
+"""
+
+__version__ = "0.1.0"
+__authors__ = 'Gherard Batisti Biffignandi, Greta Bellinzona, Greta Petazzoni, Stefano Gaiarsa'
+__credits__ = 'Gherard Batisti Biffignandi, Greta Bellinzona, Greta Petazzoni, Davide Sassera, Gian Vincenzo Zuccotti, Claudio Bandi, Fausto Baldanti, Francesco Comandatore, Stefano Gaiarsa, P-DOR, an easy-to-use pipeline to reconstruct bacterial outbreaks using genomics, Bioinformatics, 2023;, btad571, https://doi.org/10.1093/bioinformatics/btad571'
